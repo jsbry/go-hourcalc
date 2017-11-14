02dcbbf2274117a68f8905a466eac5ca02c202f0
jsbry <jsbry.biz@gmail.com>
2017-11-15 01:03:53 +0900

    cli sample
    TODO マイナス値が渡されたときの処理

--- examples/cli/cli.go
+++ examples/cli/cli.go
@@ -10,10 +10,16 @@ import (
 )
 
 var (
-	Hours = flag.String("h", "", "RSS Address")
+	Hours       = flag.String("h", "", "hh:mm形式をカンマ区切りで指定")
+	Subtraction = flag.Bool("s", false, "-sをつけると減算します")
 )
 
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, helpMessage())
+		flag.PrintDefaults()
+	}
+
 	os.Exit(Run())
 }
 
@@ -21,15 +27,29 @@ func Run() int {
 	flag.Parse()
 
 	if *Hours == "" {
-		fmt.Println(`command error ===> please hours ' -h "Hours"'`)
+		fmt.Println(`00:00`)
 		return 2
 	}
 
 	hours := strings.Split(*Hours, ",")
-	hour, err := hc.AddTime(hours)
-	if err != nil {
-		fmt.Println(err)
+	hour := "00:00"
+	if *Subtraction == false {
+		hour, _ = hc.AddTime(hours)
+	} else {
+		hour, _ = hc.DiffTime(hours)
 	}
 	fmt.Println(hour)
 	return 0
+}
+
+func helpMessage() string {
+	return `
+Usage of hourcalc:
+
+/* ja */
+hh:mm形式をカンマ区切りで渡すと時間を加減します
+
+Options
+`
+
 }
--- examples/cli/cli_test.go
+++ examples/cli/cli_test.go
@@ -2,8 +2,27 @@ package main
 
 import "testing"
 
-func TestCLI(t *testing.T) {
+func TestCLI_PASS(t *testing.T) {
 	if result := Run(); result == 2 {
+		t.Log(result)
+	} else {
+		t.Error("result :", result)
+	}
+}
+
+func TestCLI_Add(t *testing.T) {
+	*Hours = "1:00,00:01,00:02,00:03,00:04,00:05,00:06,00:07,00:08,00:09,00:10,00:11"
+	if result := Run(); result == 0 {
+		t.Log(result)
+	} else {
+		t.Error("result :", result)
+	}
+}
+
+func TestCLI_Subtraction(t *testing.T) {
+	*Hours = "1:00,00:01,00:02,00:03,00:04,00:05,00:06,00:07,00:08,00:09,-00:10,-00:11"
+	*Subtraction = true
+	if result := Run(); result == 0 {
 		t.Log(result)
 	} else {
 		t.Error("result :", result)
